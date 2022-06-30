30688ff9828d1fd54c31e3074c446ae21c386477
Savinkin Sergey <ssavinkin13@yandex.ru>
2022-06-30 17:03:34 +0300

    Add RakeFile, update Gemfile

--- app.rb
+++ app.rb
@@ -2,6 +2,16 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/ActiveRecord'
+
+set :database, { adapter: 'sqlite3', database: 'pizzashop.db' }
+
+class Product < ActiveRecord::Base 
+	validates :name, presence: true, length: { minimum: 3 }
+	validates :phone, presence: true
+	validates :datestamp, presence: true
+	validates :color, presence: true
+end	
 
 get '/' do
 	erb :index
